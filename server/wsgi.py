b608efcabba12618308a8e262d4196ee242e364e
gabyel-dev <184721807+gabyel-dev@users.noreply.github.com>
2025-12-15 11:11:51 +0800

    MOD: wsgi.py

--- server/wsgi.py
+++ server/wsgi.py
@@ -3,6 +3,8 @@ monkey.patch_all()
 
 from main import app, socketio
 
+application = socketio
+
 if __name__ == '__main__':
     print("🚀 Starting SocketIO server...")
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
