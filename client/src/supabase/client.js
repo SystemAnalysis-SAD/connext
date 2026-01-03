812314321dc8026e2816532223f846eb9b26c07f
gabyel-dev <184721807+gabyel-dev@users.noreply.github.com>
2026-01-04 06:06:19 +0800

    MINOR FIXES

--- client/src/supabase/client.js
+++ client/src/supabase/client.js
@@ -4,6 +4,3 @@ export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
 );
-
-console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL);
-console.log("SUPABASE KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
